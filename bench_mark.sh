087e78744f20a1d9a2eca5eb8871d8348281ada2
YU-WEI,HSU <weiso131@weiso131.com>
2026-04-16 19:51:19 +0800

    Use fix_mutex
    The stress-ng pthread work load is so unstable
    Use self define mutex workload.
    
    Signed-off-by: YU-WEI,HSU <weiso131@weiso131.com>

--- bench_mark.sh
+++ bench_mark.sh
@@ -3,6 +3,7 @@
 # Compile custom workloads
 gcc -o slow_timer slow_timer.c
 gcc -o random_timer random_timer.c -lm
+gcc -o fix_mutex fix_mutex.c -lpthread
 
 # Cleanup function: kill our background jobs on Ctrl+C / exit
 cleanup() {
@@ -21,13 +22,15 @@ for i in $(seq 12); do ./slow_timer & done
 # Launch random_timer x12
 for i in $(seq 12); do ./random_timer & done
 
+# Launch fix_mutex
+./fix_mutex &
+
 # stress-ng (runs in foreground, will be interrupted by Ctrl+C too)
 stress-ng \
   --cpu 12 --cpu-method fft \
   --hdd 12 --hdd-bytes 256M \
   --switch 12 \
   --timer 12 --timer-freq 500 \
-  --pthread 12 \
   --metrics-brief
 
 # If stress-ng exits on its own (e.g. --timeout), clean up the rest
